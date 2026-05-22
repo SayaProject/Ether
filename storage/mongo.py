4d0b4ca8acda4af912b17d20fa45472323550634
Learning Bots <115718228+LearningBotsOfficial@users.noreply.github.com>
2026-05-22 09:25:31 +0530

    Update mongo.py

--- storage/mongo.py
+++ storage/mongo.py
@@ -18,8 +18,7 @@
 #
 #  Thank you for respecting open-source development.
 # =============================================================================
-
-from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import AsyncMongoClient
 from config.config import Config
 from utils.logger import get_logger
 
@@ -32,7 +31,7 @@ logger = get_logger("EtherMongo")
 class EtherMongo:
     
     def __init__(self):
-        self.client: AsyncIOMotorClient = None
+        self.client: AsyncMongoClient = None
         self.db = None
     
     async def connect(self) -> bool:
@@ -41,7 +40,7 @@ class EtherMongo:
             return False
         
         try:
-            self.client = AsyncIOMotorClient(
+            self.client = AsyncMongoClient(
                 Config.MONGO_URI,
                 serverSelectionTimeoutMS=5000
             )
